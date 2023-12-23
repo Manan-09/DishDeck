50426ee0d0e8ea1cd52e973307025292b73c7b5e
Manan Solanki <mananrsolanki@gmail.com>
2023-12-23 16:30:35 +0530

    [BE] Add upload image for restaurant

--- server/src/main/java/dev/manan/dishdeck/controller/restrictedapi/RestaurantRestrictedApi.java
+++ server/src/main/java/dev/manan/dishdeck/controller/restrictedapi/RestaurantRestrictedApi.java
@@ -1,11 +1,13 @@
 package dev.manan.dishdeck.controller.restrictedapi;
 
 import dev.manan.dishdeck.data.dto.RestaurantRequestDTO;
+import dev.manan.dishdeck.data.entity.Item;
 import dev.manan.dishdeck.data.entity.Restaurant;
 import dev.manan.dishdeck.service.RestaurantService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequiredArgsConstructor
@@ -28,5 +30,10 @@ public class RestaurantRestrictedApi {
     @PutMapping("/{restaurantId}")
     public ResponseEntity<Restaurant> updateRestaurant(@RequestBody RestaurantRequestDTO requestDTO, @PathVariable String restaurantId) {
         return ResponseEntity.ok(restaurantService.updateRestaurant(requestDTO, restaurantId));
+    }
+
+    @PostMapping("/{restaurantId}/upload/image")
+    public ResponseEntity<Restaurant> uploadMoviePosterImage(@PathVariable String restaurantId, @RequestParam("file") MultipartFile file) throws Exception {
+        return ResponseEntity.ok(restaurantService.uploadImage(restaurantId, file));
     }
 }
--- server/src/main/java/dev/manan/dishdeck/service/RestaurantService.java
+++ server/src/main/java/dev/manan/dishdeck/service/RestaurantService.java
@@ -1,11 +1,15 @@
 package dev.manan.dishdeck.service;
 
 import dev.manan.dishdeck.data.dto.RestaurantRequestDTO;
+import dev.manan.dishdeck.data.entity.Item;
 import dev.manan.dishdeck.data.entity.Restaurant;
 import dev.manan.dishdeck.repo.RestaurantRepo;
 import dev.manan.dishdeck.transformer.RestaurantMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.UUID;
 
 import static dev.manan.dishdeck.service.AuthorisationHelper.validateOwnership;
 
@@ -14,6 +18,7 @@ import static dev.manan.dishdeck.service.AuthorisationHelper.validateOwnership;
 public class RestaurantService {
 
     private final RestaurantRepo restaurantRepo;
+    private final StorageService storageService;
 
     public Restaurant fetchRestaurantById(String restaurantId) {
         return  restaurantRepo.findById(restaurantId).orElseThrow();
@@ -38,5 +43,16 @@ public class RestaurantService {
         existingRestaurant = RestaurantMapper.INSTANCE.updateRestaurantFromRequest(restaurantRequestDTO, existingRestaurant);
         existingRestaurant.audit();
         return  restaurantRepo.save(existingRestaurant);
+    }
+
+    public Restaurant uploadImage(String restaurantId, MultipartFile file) throws Exception {
+        final String key = UUID.randomUUID().toString();
+        Restaurant restaurant = fetchRestaurantById(restaurantId);
+        validateOwnership(restaurant);
+        final String oldImageKey = restaurant.getImage();
+        storageService.uploadImage(key, file);
+        restaurant.setImage(key);
+        storageService.deleteObject(oldImageKey);
+        return restaurantRepo.save(restaurant);
     }
 }
